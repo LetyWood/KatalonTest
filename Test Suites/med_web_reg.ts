1e41189ded2b4a22a6382f120138ceebf90c5d44
LENOVO THINKPAD <LENOVO THINKPAD@192.168.0.24>
2024-01-02 09:48:05 -0600

    automated

---
+++ Test Suites/med_web_reg.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>This is the first terst suite, like a test</description>
+   <name>med_web_reg</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>3c83c6bd-70d3-4bf0-9798-f0ba83fbbc74</testSuiteGuid>
+   <testCaseLink>
+      <guid>c57282b4-e2e9-408a-b5ed-8e2c9ce61457</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Purdygo Web</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
